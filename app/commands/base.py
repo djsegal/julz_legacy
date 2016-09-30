a045a9b5ce2c566f8a362ff954d258d6f81010aa
djsegal <djsegal@wisc.edu>
2016-09-30 00:32:30 -0400

    Extract reusable code from 'new' into base command

--- app/commands/base.py
+++ app/commands/base.py
@@ -1,5 +1,12 @@
 """The base command."""
 
+import jinja2
+
+import re
+
+from os import makedirs, path
+
+from shutil import rmtree
 
 class Base(object):
   """A base command."""
@@ -11,6 +18,29 @@ class Base(object):
 
   def run(self):
     raise NotImplementedError('You must implement the run() method yourself!')
+
+  def openFile(self, curDir, curFile, autoClose=False, depth=2, verbose=True):
+    openedFile = open('/'.join([curDir, curFile]), 'a')
+    if verbose: self.printBullet(self.getLastChunk(curFile), depth)
+    if not autoClose : return openedFile
+    openedFile.close()
+
+  def makeSubDir(self, subDir, depth=1):
+    makedirs(subDir)
+    self.openFile(subDir, '.keep', True, 2, False)
+    self.printBullet(self.getLastChunk(subDir), depth)
+
+  def getLastChunk(self, curItem):
+    if ( '/' not in curItem ) : return curItem
+    return curItem.rsplit('/', 1)[-1]
+
+  def printHeader(self, header):
+    print "\nMaking %s:" % header
+
+  def printBullet(self, bullet, depth):
+    bulletSymbol = '-' if '.jl' in bullet else '+'
+    prefix = '  ' * depth + bulletSymbol + ' '
+    print "%s%s" % ( prefix, bullet )
 
   def getRelativePath(self):
     pathParam = '--path'
--- app/commands/new.py
+++ app/commands/new.py
@@ -1,16 +1,6 @@
 """The new command."""
 
-import jinja2
-
-import re
-
-from json import dumps
-
 from .base import Base
-
-from os import makedirs, path
-
-from shutil import rmtree
 
 class New(Base):
   """Start new Julia project"""
@@ -93,26 +83,3 @@ class New(Base):
     defaultEnvironments = 'development test production'.split()
     for curEnvironment in defaultEnvironments:
       self.openFile(environmentsDir, '%s.jl' % curEnvironment, True, 3)
-
-  def makeSubDir(self, subDir, depth=1):
-    makedirs(subDir)
-    self.openFile(subDir, '.keep', True, 2, False)
-    self.printBullet(self.getLastChunk(subDir), depth)
-
-  def getLastChunk(self, curItem):
-    if ( '/' not in curItem ) : return curItem
-    return curItem.rsplit('/', 1)[-1]
-
-  def printHeader(self, header):
-    print "\nMaking %s:" % header
-
-  def openFile(self, curDir, curFile, autoClose=False, depth=2, verbose=True):
-    openedFile = open('/'.join([curDir, curFile]), 'a')
-    if verbose: self.printBullet(self.getLastChunk(curFile), depth)
-    if not autoClose : return openedFile
-    openedFile.close()
-
-  def printBullet(self, bullet, depth):
-    bulletSymbol = '-' if '.jl' in bullet else '+'
-    prefix = '  ' * depth + bulletSymbol + ' '
-    print "%s%s" % ( prefix, bullet )
