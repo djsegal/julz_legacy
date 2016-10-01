d80f5217f9143e5aca18c030f50469b970e430ae
djsegal <djsegal@wisc.edu>
2016-10-01 02:49:42 -0400

    Flesh out update command w/ simple piped pkg call

--- app/commands/update.py
+++ app/commands/update.py
@@ -4,3 +4,9 @@ from .base import *
 
 class Update(Base):
   """Update Julia package(s)"""
+
+  def run(self):
+    self.printHeader('gems', 'Updating')
+    print ''
+
+    call('julia -e "Pkg.update()"', shell=True)
