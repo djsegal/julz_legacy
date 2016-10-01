882e2352375a1f574bc7031841fb984306605f2b
djsegal <djsegal@wisc.edu>
2016-10-01 02:57:30 -0400

    Flesh out install method to use Gemfile (woo!)

--- app/commands/install.py
+++ app/commands/install.py
@@ -4,3 +4,26 @@ from .base import *
 
 class Install(Base):
   """Install Julia package(s)"""
+
+  def run(self):
+    self.printHeader('gems', 'Installing')
+    print ''
+
+    f = open('./Gemfile', 'r')
+    for line in f:
+      line = line.strip().replace('"', '')
+      splitLine = line.split()
+
+      if len(splitLine) == 0 or splitLine[0] != 'gem': continue
+      splitLine.pop(0)
+
+      if len(splitLine) > 1:
+        print("Versioning is not currently implemented")
+        return
+
+      curGem = splitLine[0]
+      self.printBullet(curGem)
+      call("julia -e 'Pkg.add(\"%s\")'" % curGem, shell=True)
+      self.eraseLines()
+
+    print ''
